3b02049db11c9342db934476f6135055944555aa
lazycoder2021 <tksingh2087@gmail.com>
2021-02-09 16:45:38 +0530

    Initial Comitt

---
+++ src/components/layout/Header.js
@@ -0,0 +1,32 @@
+import React from 'react'; 
+import { Link } from 'react-router-dom'; 
+
+function Header() {
+    return (
+        <header style={headerStyle}>
+            <h1>TodoList</h1>
+            <Link style={linkStyle} to="/">Home</Link> | <Link style={linkStyle} to="/about">About</Link>
+        </header>
+        
+    )
+}
+
+const headerStyle = {
+    background: 'gold', 
+    color: 'black', 
+    textAlign: 'center', 
+    padding: '10px'
+}
+
+const linkStyle = {
+    color: 'black', 
+    textDecoration: 'none'
+}
+
+
+
+export default Header; 
+
+
+
+// JavaScript source code
